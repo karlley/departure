ee32cfa0e38c762c64f11e2be1ae0eca031778bb
karlley <karlley.jp@gmail.com>
2020-06-01 05:51:40 +0900

    email unique マイグレーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_200854) do
+ActiveRecord::Schema.define(version: 2020_05_31_204125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_31_200854) do
     t.string "nationality"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
