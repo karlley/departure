b1841960dbd45b066d71a2485c2753e6e3989751
karlley <karlley.jp@gmail.com>
2020-06-04 06:01:48 +0900

    User model System Spec 追加

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "StaticPages", type: :system do
         expect(page).to have_content "Departure"
       end
 
-      it "正しいタイトルが表示されている事を確認" do
+      it "正しいタイトル表示を確認" do
         expect(page).to have_title full_title
       end
     end
@@ -22,11 +22,11 @@ RSpec.describe "StaticPages", type: :system do
       visit about_path
     end
 
-    it "What's Departure?の文字列の存在を確認" do
+    it "What's Departure? の文字列の存在を確認" do
       expect(page).to have_content "What's Departure?"
     end
 
-    it "正しいタイトルが表示されている事を確認" do
+    it "正しいタイトル表示を確認" do
       expect(page).to have_title full_title("About")
     end
   end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  describe "Users", type: :system do
+    before do
+      visit signup_path
+    end
+
+    context "ページレイアウト" do
+      it "Signup の文字列の存在を確認" do
+        expect(page).to have_content 'Signup'
+      end
+
+      it "正しいタイトル表示を確認" do
+        expect(page).to have_title full_title('Signup')
+      end
+
+    end
+  end
+end
