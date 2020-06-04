51c404b2f2499509acc52ab672de672d81c3080d
karlley <karlley.jp@gmail.com>
2020-06-05 06:05:20 +0900

    テストデータ作成用 gem 設定

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    sequence(:email) { |n| "example#{n}@example.com" }
+    password { "foobar" }
+    password_confirmation { "foobar" }
+    nationality { "Japan" }
+    introduction { "Hello! My name is Faker!" }
+  end
+end
