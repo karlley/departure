a04c9626028d3b31ffa0a3ace8d48ed1243fd5f6
karlley <karlley.jp@gmail.com>
2020-07-05 05:00:18 +0900

    Add: Destination validates
    :user_id, :name, :country, :description にバリデーション追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,7 @@
 class Destination < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true, lingth: { maximum: 50 }
+  validates :country, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 140 }
 end
