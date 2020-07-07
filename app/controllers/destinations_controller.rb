6221c28685a09432effb965f93626a2c2464f7a0
karlley <karlley.jp@gmail.com>
2020-07-07 17:54:43 +0900

    Add: destinations new
    行き先登録ページ 作成

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,7 @@
 class DestinationsController < ApplicationController
+  before_action :logged_in_user
+
+  def new
+    @destination = Destination.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get :about, to: 'static_pages#about'
   get :signup, to: 'users#new'
   resources :users
+  resources :destinations
   get :login, to: 'sessions#new'
   post :login, to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
