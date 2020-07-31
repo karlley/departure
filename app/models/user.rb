56660fb739d96a41359c8e61e1e0acc58e50cd86
karlley <karlley.jp@gmail.com>
2020-08-01 05:10:24 +0900

    Add: 受動的なfollow 機能追加
    passive_relationships 関連付け, :followed_by? メソッド 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :destinations, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
@@ -59,6 +61,11 @@ class User < ApplicationRecord
   # 現在のユーザーがフォローしていたらtrue を返す
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  # 現在のユーザーがフォローされていたらtrue を返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 
   private
