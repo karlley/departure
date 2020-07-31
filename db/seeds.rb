df6faf4d5b62941c65df23830e2d703e3943f860
karlley <karlley.jp@gmail.com>
2020-08-01 05:41:35 +0900

    Add: follow サンプルデータ 追加
    seeds 追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Admin User
 User.create!(name: "Example User",
              email: "sample@example.com",
              password: "password",
@@ -6,6 +7,7 @@ User.create!(name: "Example User",
              nationality: "Japan",
              admin: true)
 
+# User
 99.times do |n|
   name = Faker::Name.name
   email = "sample-#{n+1}@example.com"
@@ -18,9 +20,18 @@ User.create!(name: "Example User",
                nationality: Faker::Address.country)
 end
 
+# Destination
 10.times do |n|
   Destination.create!(name: Faker::Address.city,
                       description: "This Destination is Faker!",
                       country: Faker::Address.country,
                       user_id: 1)
 end
+
+# Relationship
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
