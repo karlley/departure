f12bf3875a1d33c2f1698597165e17a61f11a1e1
karlley <karlley.jp@gmail.com>
2020-08-04 05:03:04 +0900

    Add: Follow, Followed ボタンを表示
    ボタン表示用パーシャル, ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
   end
   resources :destinations
+  resources :relationships, only: [:create, :destroy]
   get :login, to: 'sessions#new'
   post :login, to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
