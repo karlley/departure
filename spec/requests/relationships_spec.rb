7fc9590ab1ae5f654ce6b3d5077af21b744dc498
karlley <karlley.jp@gmail.com>
2020-08-05 12:06:44 +0900

    Add: rerationship requests spec 追加
    未ログイン時のcreate, destory アクション の動作テスト

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -13,5 +13,19 @@ RSpec.describe "ユーザーフォロー機能", type: :request do
       get followers_user_path(user)
       expect(response).to redirect_to login_path
     end
+
+    it "create は実行不可, Login ページへリダイレクトすること" do
+      expect do
+        post relationships_path
+      end.not_to change(Relationship, :count)
+      expect(response).to redirect_to login_path
+    end
+
+    it "destroy は実行不可, Login ページへリダイレクトすること" do
+      expect do
+        delete relationship_path(user)
+      end.not_to change(Relationship, :count)
+      expect(response).to redirect_to login_path
+    end
   end
 end
