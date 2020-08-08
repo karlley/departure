c87d16bd840d9be80be411cc68796590397cf04b
karlley <karlley.jp@gmail.com>
2020-08-09 06:15:11 +0900

    Add: favarite コントローラ create, destory 追加
    Ajax を使用したcreate, destory アクションを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @destination = Destination.find(params[:destination_id])
+    @user = @destinaton.user
+    current_user.favorite(@destination)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @destination = Destination.find(params[:destination_id])
+    current_user.favorites.find_by(destination_id: @destination.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
