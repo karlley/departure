9c0d9eb813cbeb3807e119cae631d4eddbea331f
karlley <karlley.jp@gmail.com>
2020-08-14 05:57:27 +0900

    Add: favorites 一覧ページ 追加
    controller, routes, view, CSS を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :logged_in_user
 
+  def index
+    @favorites = current_user.favorites
+  end
+
   def create
     @destination = Destination.find(params[:destination_id])
     @user = @destination.user
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :destination
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :destination_id, presence: true
 end
