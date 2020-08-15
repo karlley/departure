93145ca71554d891f4b7f3fb1a0f035aedd401b5
karlley <karlley.jp@gmail.com>
2020-08-16 06:16:59 +0900

    Add: Comment model spec, factory 追加
    Comment モデルのバリデーションテスト

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    user_id 1
-    destination_id 1
-    content "MyText"
+    user_id { 1 }
+    content { "This is a comment!" }
+    association :destination
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:comment) { create(:comment) }
+
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(comment).to be_valid
+    end
+
+    it "user_id がnil で無効な状態であること" do
+      comment = build(:comment, user_id: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "destination_id がnil で無効な状態であること" do
+      comment = build(:comment, destination_id: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "content がnil で無効な状態であること" do
+      comment = build(:comment, content: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "content が50文字以内であること" do
+      comment = build(:comment, content: "a" * 51)
+      comment.valid?
+      expect(comment.errors[:content]).to include("は50文字以内で入力してください")
+    end
+  end
 end
