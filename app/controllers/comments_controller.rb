b224e30d33f699a6f3d61e3a97b7a95b29708ad2
karlley <karlley.jp@gmail.com>
2020-08-17 06:10:58 +0900

    Add: comments コントローラ create 追加
    空のcreate アクションにコメント処理を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,15 @@ class CommentsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @destination = Destination.find(params[:destination_id])
+    @user = @destination.user
+    @comment = @destination.comments.build(user_id: current_user.id, content: params[:comment][:content])
+    if !@destination.nil? && @comment.save
+      flash[:success] = "Added a comment!"
+    else
+      flash[:danger] = "Empty comment can't be posted!"
+    end
+    redirect_to request.referrer || root_url
   end
 
   def destroy
