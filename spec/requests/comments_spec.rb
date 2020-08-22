2d2c111694513c85965bf632c1a29ea311960db6
karlley <karlley.jp@gmail.com>
2020-08-23 05:35:39 +0900

    Add: comments requests spec 追加
    ログイン状態別でのコメントの登録/削除の動作テスト

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "コメント機能", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
   let!(:destination) { create(:destination) }
   let!(:comment) { create(:comment, user_id: user.id, destination: destination) }
 
   describe "コメントの登録" do
     context "ログイン済みの場合" do
+      before do
+        login_for_request(user)
+      end
+
+      it "有効な内容のコメントが登録できること" do
+        expect do
+          post comments_path, params: { destination_id: destination.id, comment: { content: "This is comment!" } }
+        end.to change(destination.comments, :count).by(1)
+      end
+
+      it "無効な内容のコメントが登録できないこと" do
+        expect do
+          post comments_path, params: { destination_id: destination.id, comment: { content: "" } }
+        end.not_to change(destination.comments, :count)
+      end
     end
 
     context "未ログインの場合" do
@@ -24,6 +40,23 @@ RSpec.describe "コメント機能", type: :request do
 
   describe "コメントの削除" do
     context "ログイン済みの場合" do
+      context "コメントを作成したユーザーである場合" do
+        it "コメントの削除ができること" do
+          login_for_request(user)
+          expect do
+            delete comment_path(comment)
+          end.to change(destination.comments, :count).by(-1)
+        end
+      end
+
+      context "コメントを作成したユーザーでない場合" do
+        it "コメントの削除はできないこと" do
+          login_for_request(other_user)
+          expect do
+            delete comment_path(comment)
+          end.not_to change(destination.comments, :count)
+        end
+      end
     end
 
     context "未ログインの場合" do
