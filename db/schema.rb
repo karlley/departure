e0757450339c2bd4d85893c0e3ff7cc2e0cbf6ab
karlley <karlley.jp@gmail.com>
2020-08-24 06:17:05 +0900

    Add: Notification マイグレーション追加
    notifications, user_id にadd_index 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_201742) do
+ActiveRecord::Schema.define(version: 2020_08_23_210419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,17 @@ ActiveRecord::Schema.define(version: 2020_08_15_201742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "destination_id"], name: "index_favorites_on_user_id_and_destination_id", unique: true
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "destination_id"
+    t.integer "from_user_id"
+    t.text "content"
+    t.integer "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
