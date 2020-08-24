b2a5359ccf8282480824056129bc4870827ba1cd
karlley <karlley.jp@gmail.com>
2020-08-25 05:35:59 +0900

    Add: notifications ルーティング追加
    recources を使ったルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'notifications/index'
   root "static_pages#home"
   get :about, to: "static_pages#about"
   get :signup, to: "users#new"
@@ -17,4 +16,5 @@ Rails.application.routes.draw do
   post "favorites/:destination_id/create" => "favorites#create"
   delete "favorites/:destination_id/destroy" => "favorites#destroy"
   resources :comments, only: [:create, :destroy]
+  resources :notifications, only: :index
 end
