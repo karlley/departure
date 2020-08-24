3ae8ca83a1d68cb2031188c38804966e88674258
karlley <karlley.jp@gmail.com>
2020-08-25 05:00:01 +0900

    Add: Notification, User 関連付け, バリデーション追加
    バリデーション, 降順の設定, アソシエーションを追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,8 @@
 class Notification < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :destination_id, presence: true
+  validates :from_user_id, presence: true
+  validates :type, presence: true
+  default_scope -> { order(created_at: :desc) }
 end
