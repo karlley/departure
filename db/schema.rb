4fc95c7207dec8aa4a14a71471089e1285caffd0
karlley <karlley.jp@gmail.com>
2020-08-25 05:59:18 +0900

    Add: User model notification カラムを追加
    ユーザー毎に通知の有無を判別するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_210419) do
+ActiveRecord::Schema.define(version: 2020_08_24_205420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_210419) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.boolean "notification", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
