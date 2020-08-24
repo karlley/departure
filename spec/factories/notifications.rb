e45a9829f0345fe81696b005487d2f30cb7474a0
karlley <karlley.jp@gmail.com>
2020-08-25 05:19:24 +0900

    Add: Notification factory, model spec 追加
    Notification バリデーションテストを追加

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
   factory :notification do
     user_id 1
     destination_id 1
-    from_user_id 1
-    content "MyText"
+    from_user_id 2
+    content ""
     type 1
+    association :user
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:notification) { create(:notification) }
+
+  it "notification インスタンスが有効であること" do
+    expect(notification).to be_valid
+  end
+
+  it "user_id がnil の場合, 無効であること" do
+    notification.user_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "destination_id がnil の場合, 無効であること" do
+    notification.destination_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "from_user_id がnil の場合, 無効であること" do
+    notification.from_user_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "type がnil の場合, 無効であること" do
+    notification.type = nil
+    expect(notification).not_to be_valid
+  end
 end
