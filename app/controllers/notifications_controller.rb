1f1b9d2c97fe38170574d72ff0507e724dc008ed
karlley <karlley.jp@gmail.com>
2020-08-29 05:31:03 +0900

    Add: notifications index ページ追加
    index アクション追加, index 用パーシャル追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,9 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
 
   def index
+    # current_user の通知を取得
+    @notifications = current_user.notifications
+    # Notifications ページで通知確認後に通知フラグを削除
+    current_user.update_attribute(:notification, false)
   end
 end
