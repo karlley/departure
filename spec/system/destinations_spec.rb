5f4dda04f971473d7c6dc697c868547ac16448d9
karlley <karlley.jp@gmail.com>
2020-10-01 04:26:49 +0900

    Add: destinations system spec GoogleMap 動作テスト
    new 入力部, show 出力部 のテストを追加

--- spec/system/destinations_spec.rb
+++ spec/system/destinations_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe "Destinations", type: :system do
       it "入力フォームに正しいラベルが表示されること" do
         expect(page).to have_content "行き先名"
         expect(page).to have_content "説明"
+        expect(page).to have_content "スポット"
         expect(page).to have_content "国"
       end
     end
@@ -32,6 +33,7 @@ RSpec.describe "Destinations", type: :system do
       it "有効なデータで登録処理で成功のフラッシュが表示されること" do
         fill_in "行き先名", with: "サンプルの行き先"
         fill_in "説明", with: "サンプルの行き先の説明です"
+        fill_in "スポット", with: "サンプルの行き先のスポット"
         fill_in "国", with: "Japan"
         attach_file "destination[picture]", "#{Rails.root}/spec/fixtures/test_destination_1.jpg"
         click_button "登録する"
@@ -41,6 +43,7 @@ RSpec.describe "Destinations", type: :system do
       it "画像無しの登録はデフォルト画像が設定されること" do
         fill_in "行き先名", with: "サンプルの行き先"
         fill_in "説明", with: "サンプルの行き先の説明です"
+        fill_in "スポット", with: "サンプルの行き先のスポット"
         fill_in "国", with: "Japan"
         click_button "登録する"
         expect(page).to have_link(href: destination_path(Destination.first))
@@ -49,6 +52,7 @@ RSpec.describe "Destinations", type: :system do
       it "無効なデータで登録処理で失敗のフラッシュが表示されること" do
         fill_in "行き先名", with: ""
         fill_in "説明", with: "サンプルの行き先の説明です"
+        fill_in "スポット", with: "サンプルの行き先のスポット"
         fill_in "国", with: "Japan"
         click_button "登録する"
         expect(page).to have_content "行き先名を入力してください"
@@ -76,6 +80,41 @@ RSpec.describe "Destinations", type: :system do
         expect(page).to have_content destination.country
         expect(page).to have_content destination.description
         expect(page).to have_link nil, href: destination_path(destination), class: "destination-picture"
+      end
+
+      it "GoogleMap が表示されていること", js: true do
+        expect(page).to have_css "div.gm-style"
+      end
+    end
+
+    context "GoogleMap 表示内容", js: true do
+      before do
+        login_for_system(user)
+        visit destination_path(destination)
+      end
+
+      it "ピンが表示されていること" do
+        expect(page).to have_selector "img[src$='spotlight-poi2_hdpi.png']"
+      end
+
+      it "infowindow が表示されること" do
+        # map#gmimap0 要素の高さが0pxなのでvisible: false で対応
+        pin = find("map#gmimap0 area", visible: false)
+        pin.click
+        expect(page).to have_css "div.infowindow"
+      end
+
+      it "infowindow に行き先情報が表示されること" do
+        pin = find("map#gmimap0 area", visible: false)
+        pin.click
+        expect(page).to have_content destination.name
+        expect(page).to have_content destination.spot
+        expect(page).to have_content destination.country
+        expect(page).to have_content destination.address
+        expect(page).to have_link "GoogleMap で見る", href: "https://maps.google.co.jp/maps?q=loc:#{destination.latitude},#{destination.longitude}&iwloc=J", class: "googlemap-link"
+        link = find(".googlemap-link")
+        # googelmap へのリンクにtarget:"_blank"属性が付いているか
+        expect(link[:target]).to eq "_blank"
       end
     end
 
