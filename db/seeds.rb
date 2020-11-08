0a17aa3695492bc6a7c821928cbd432ea6b1f751
karlley <karlley.jp@gmail.com>
2020-11-09 06:22:43 +0900

    Fix: seed ファイル CSV インポート処理 追加
    Airline CSV インポートの処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,12 +46,23 @@ followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
-# Country CSV
+# Country CSV import
 require "csv"
 
 CSV.foreach("country.csv", headers: true) do |row|
   Country.create!(
     country_name: row["国・地域名"],
     region: row["場所"]
+  )
+end
+
+# Airline CSV import
+
+CSV.foreach("airline.csv", headers: true) do |row|
+  Airline.create!(
+    airline_name: row["航空会社"],
+    country_id: row["国番号"],
+    alliance: row["アライアンス"],
+    alliance_type: row["アライアンス種別"]
   )
 end
