e9fac1ee2aba0cc718d903cc43c9c80a9f5ec3de
karlley <karlley.jp@gmail.com>
2020-11-19 12:12:01 +0900

    Fix: rubocop による軽微な修正
    自動修正部分のみコミット

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -10,28 +10,28 @@ User.create!(name: "Example User",
 # User
 99.times do |n|
   name = Faker::Name.name
-  email = "sample-#{n+1}@example.com"
+  email = "sample-#{n + 1}@example.com"
   password = "password"
   User.create!(name: name,
                email: email,
                password: password,
                password_confirmation: password,
-               introduction: "My name is Faker No.#{n+1}!",
+               introduction: "My name is Faker No.#{n + 1}!",
                nationality: Faker::Address.country)
 end
 
 # Destination
 10.times do |n|
   Destination.create!(name: Faker::Address.city,
-                      description: "This is Faker Destination No.#{n+1}!",
+                      description: "This is Faker Destination No.#{n + 1}!",
                       country: Faker::Address.country,
                       spot: Faker::Address.street_name,
                       latitude: Faker::Address.latitude,
                       longitude: Faker::Address.longitude,
                       # address: Geocoder.search([:latitude, :longitude]).first.address,
                       address: Faker::Address.full_address,
-                      expense: Faker::Number.between(from:1, to: 100) * 10000,
-                      season: Faker::Number.between(from:1, to: 12),
+                      expense: Faker::Number.between(from: 1, to: 100) * 10000,
+                      season: Faker::Number.between(from: 1, to: 12),
                       experience: "Your Experience!",
                       airline: "Select Airline Company!",
                       food: Faker::Food.dish,
