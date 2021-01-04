87013e373b969d8c154a6849704b83bf9ef7918c
karlley <karlley.jp@gmail.com>
2021-01-05 05:23:13 +0900

    Add: Country, Airline model spec 追加
    バリデーションテストを追加

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:airline) { create(:airline) }
+
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(airline).to be_valid
+    end
+
+    it "airline_name が無いと無効であること" do
+      airline = build(:airline, airline_name: nil)
+      expect(airline).not_to be_valid
+    end
+
+    it "country_id が無いと無効であること" do
+      airline = build(:airline, country_id: nil)
+      expect(airline).not_to be_valid
+    end
+
+    it "alliance が無いと無効であること" do
+      airline = build(:airline, alliance: nil)
+      expect(airline).not_to be_valid
+    end
+
+    it "alliance_type が無いと無効であること" do
+      airline = build(:airline, alliance_type: nil)
+      expect(airline).not_to be_valid
+    end
+  end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:country) { create(:country) }
+
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(country).to be_valid
+    end
+
+    it "country_name が無い場合は無効な状態であること" do
+      country = build(:country, country_name: nil)
+      expect(country).not_to be_valid
+    end
+
+    it "region が無い場合は無効な状態で有ること" do
+      country = build(:country, region: nil)
+      expect(country).not_to be_valid
+    end
+  end
 end
