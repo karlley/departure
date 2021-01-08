e6b4d34a033531faad1c195d335a80886592bff5
karlley <karlley.jp@gmail.com>
2021-01-08 20:36:57 +0900

    Improve: Destination show/edit パーシャル共通化
    ヘルパーメソッド化してパーシャルを共通化

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -1,2 +1,11 @@
 module DestinationsHelper
+  # edit ページが表示されていればtrue を返す
+  def edit_page?
+    @destination.id.present?
+  end
+
+  # 画像が登録されているばtrue を返す
+  def have_picture?
+    @destination.picture.url.present?
+  end
 end
