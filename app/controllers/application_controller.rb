6f3e8e880b5b7aa083338cd5c211dd00239ef0c8
karlley <karlley.jp@gmail.com>
2021-02-11 14:33:41 +0900

    Fix: application controller set_search 修正
    @destinations, @search_word の値を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # 全てのアクションで@q, @destinations, @search_word がセットする
+  # 全てのアクションでset_search メソッドを実行する
   before_action :set_search
   # CSRF対策
   protect_from_forgery with: :exception
@@ -8,22 +8,20 @@ class ApplicationController < ActionController::Base
   # have_search_word? を使えるようにする
   include ApplicationHelper
 
-  # フィードから検索条件に該当する行き先を検索
+  # feed から検索条件に該当する行き先を検索
   def set_search
-    if logged_in? && have_search_word?
-      # 検索ワードからスペース区切りで配列を作成
-      search_word = params[:q][:name_or_spot_or_address_cont].split(/[\p{blank}\s]+/)
-      # 検索ワードの数だけ検索ワードをkey にしたハッシュを作成
-      grouping_hash = search_word.reduce({}) { |hash, word| hash.merge(word => { name_or_spot_or_address_cont: word }) }
-      # ユーザのフィードから検索
+    if logged_in?
+      if have_search_word?
+        # 検索ワードからスペース区切りで配列を作成
+        # 検索ワードの数だけ検索ワードをkey にしたハッシュを作成
+        grouping_hash = params[:q][:name_or_spot_or_address_cont].split(/[\p{blank}\s]+/).reduce({}) { |hash, word| hash.merge(word => { name_or_spot_or_address_cont: word }) }
+        # view h1 表示用にインスタンス変数化
+        @search_word = params[:q][:name_or_spot_or_address_cont]
+      end
+      # ユーザのフィードから検索ワード検索/全件取得
       @q = current_user.feed.paginate(page: params[:page], per_page: 5).ransack({ combinator: 'or', groupings: grouping_hash })
       # 検索結果(distinct: true で重複除外)
       @destinations = @q.result(distinct: true)
-      # view タイトル表示ようにインスタンス変数化
-      @search_word = search_word
-    else
-      # ransack メソッドを使って@q を取得しないとNo Ransack::Search エラーが出る
-      @q = current_user.feed.paginate(page: params[:page], per_page: 5).ransack(params[:q])
     end
   end
 
