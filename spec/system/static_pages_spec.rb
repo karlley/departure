bb419dac08730721bdc36f6558467f76888e0134
karlley <karlley.jp@gmail.com>
2021-04-02 14:12:54 +0900

    Add: 投稿一覧の横並びに伴う表示テスト 追加
    投稿内容の表示テストを追加

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -2,48 +2,59 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :system do
   describe "Home ページ" do
-    context "ページ全体" do
-      before do
-        visit root_path
-      end
+    context "ページレイアウト" do
+      context "非ログイン時" do
+        before do
+          visit root_path
+        end
 
-      it "Departure の文字列の存在を確認" do
-        expect(page).to have_content "Departure"
-      end
+        it "Departure の文字列の存在を確認" do
+          expect(page).to have_content "Departure"
+        end
 
-      it "正しいタイトル表示を確認" do
-        expect(page).to have_title full_title
-      end
-    end
-
-    context "行き先フィード", js: true do
-      let!(:user) { create(:user) }
-      let!(:destination) { create(:destination, user: user) }
-
-      before do
-        login_for_system(user)
-      end
-
-      it "行き先のページネーションの表示を確認" do
-        create_list(:destination, 13, user: user)
-        visit root_path
-        expect(page).to have_content "All Destination (#{user.destinations.count})"
-        expect(page).to have_css "div.pagination"
-        Destination.take(12).each do |d|
-          expect(page).to have_link d.name
+        it "正しいタイトル表示を確認" do
+          expect(page).to have_title full_title
         end
       end
 
-      it "'New Destination' ボタンの表示を確認" do
-        visit root_path
-        expect(page).to have_link "New Destination", href: new_destination_path
-      end
+      context "ログイン時" do
+        let!(:user) { create(:user) }
+        let!(:destination) { create(:destination, :picture, user: user) }
 
-      it "行き先削除後、削除成功のフラッシュの表示を確認" do
-        visit root_path
-        click_on "delete"
-        page.driver.browser.switch_to.alert.accept
-        expect(page).to have_content "Destination deleted!"
+        before do
+          login_for_system(user)
+          visit root_path
+        end
+
+        it "行き先のページネーションの表示を確認" do
+          # 行き先を13件作成
+          create_list(:destination, 13, user: user)
+          visit root_path
+          expect(page).to have_content "All Destination (#{user.destinations.count})"
+          expect(page).to have_css "div.pagination"
+          Destination.take(12).each do |destination|
+            expect(page).to have_link destination.name
+          end
+        end
+
+        it "'New Destination' ボタンの表示を確認" do
+          expect(page).to have_link "New Destination", href: new_destination_path
+        end
+
+        it "行き先の情報が正しく表示されている事を確認" do
+          expect(page).to have_selector "img[src$='test_destination_1.jpg']"
+          expect(page).to have_link nil, href: destination_path(destination), class: "destination-picture-link"
+          expect(page).to have_css "img.gravatar"
+          expect(page).to have_link nil, href: user_path(user), class: "user-icon-link"
+          expect(page).to have_link destination.name, href: destination_path(destination)
+          expect(page).to have_link destination.user.name, href: user_path(user)
+          # 国名を取得
+          country_name = get_country_name(destination)
+          expect(page).to have_content country_name
+          expect(page).to have_css ".fa-heart"
+          expect(page).to have_content "いいね!"
+          expect(page).to have_css "span.timestamp"
+        end
       end
     end
   end
