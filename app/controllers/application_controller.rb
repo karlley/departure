34d12c23fa3795d33a3179a30a804a3874e95ad1
karlley <karlley.jp@gmail.com>
2021-05-14 12:32:18 +0900

    Add: destinations#index マップ表示用メソッド 追加
    application_controller にset_markers メソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  # 全てのアクションでset_search メソッドを実行する
+  # 全てのアクションで下記メソッドを実行
   before_action :set_search
+  before_action :set_markers
   # CSRF対策
   protect_from_forgery with: :exception
   # logged_in? を使えるようにする
@@ -9,7 +10,7 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   # TODO: 条件検索も追加
-  # feed から検索条件に該当する行き先を検索
+  # フィード から検索条件に該当する行き先を検索
   def set_search
     if logged_in?
       if have_search_word?
@@ -23,9 +24,18 @@ class ApplicationController < ActionController::Base
         grouping_hash = nil
       end
       # ユーザのフィードから検索ワード検索/全件取得
-      @q = current_user.feed.paginate(page: params[:page], per_page: 5).ransack({ combinator: 'or', groupings: grouping_hash })
+      @q = current_user.feed.paginate(page: params[:page], per_page: 12).ransack({ combinator: 'or', groupings: grouping_hash })
       # 検索結果(distinct: true で重複除外)
       @destinations = @q.result(distinct: true)
+    end
+  end
+
+  # 検索結果からGoogleMap 表示用のマーカーを作成
+  def set_markers
+    @markers = Gmaps4rails.build_markers(@destinations) do |destination, marker|
+      marker.lat(destination.latitude)
+      marker.lng(destination.longitude)
+      marker.infowindow render_to_string(partial: "destinations/map_infowindow", locals: { destination: destination })
     end
   end
 
