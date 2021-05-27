8d49b517819c42f8886fc9cac3f61adfb2225a38
karlley <karlley.jp@gmail.com>
2021-05-28 06:22:53 +0900

    Add: destinations#edit  ヘルパーメソッド追加
    削除ボタン表示用にedit_page? ヘルパーメソッドを追加

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -4,6 +4,11 @@ module DestinationsHelper
     @destination.id.present? && @destination.picture.url.present?
   end
 
+  # destinations#edit でedit ページでtrue を返す
+  def edit_page?
+    @destination.id.present?
+  end
+
   # 国番号から国名を取得
   def get_country_name(destination)
     Country.find_by(id: destination.country).country_name
