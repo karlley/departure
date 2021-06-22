d8d00d8e5185f7a370e13cd8071a057140d04d7f
karlley <karlley.jp@gmail.com>
2021-06-23 05:58:00 +0900

    Fix: users#following/followers ファイル名等 変更
    パーシャル名、変数名を変更、不要部を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,14 +62,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @following_follower_users = @user.following.paginate(page: params[:page])
     render "show_follow"
   end
 
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @following_follower_users = @user.followers.paginate(page: params[:page])
     render "show_follow"
   end
 
