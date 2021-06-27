816c75b837743dfcec4ccd073ffaeab7503d7cb4
karlley <karlley.jp@gmail.com>
2021-06-28 07:33:39 +0900

    Fix: relationship system spec 表示テスト 修正
    表示変更にあせてた修正, users#follower タイトル修正

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -1,88 +1,102 @@
 require 'rails_helper'
 
 RSpec.describe "Relationships", type: :system do
-  let!(:user1) { create(:user) }
-  let!(:user2) { create(:user) }
-  let!(:user3) { create(:user) }
-  let!(:user4) { create(:user) }
-  let!(:destination1) { create(:destination, user: user1) }
-  let!(:destination2) { create(:destination, user: user2) }
-  let!(:destination3) { create(:destination, user: user3) }
-
   describe "users#following" do
-    before do
-      create(:relationship, follower_id: user1.id, followed_id: user2.id)
-      create(:relationship, follower_id: user1.id, followed_id: user3.id)
-      login_for_system(user1)
-      visit following_user_path(user1)
-    end
+    context "ログインユーザがフォロー中一覧ページにアクセスした場合" do
+      let(:user1) { create(:user) }
+      let(:user2) { create(:user) }
 
-    context "ページレイアウト" do
-      it "'Following'の文字列が存在すること" do
-        expect(page).to have_content "Following"
+      before do
+        create(:relationship, follower_id: user1.id, followed_id: user2.id)
+        login_for_system(user1)
+        visit following_user_path(user1)
+      end
+
+      it "'Following / Followers'が表示される" do
+        expect(page).to have_content "Following / Followers"
+      end
+
+      it "users#followers へのリンクが表示される" do
+        expect(page).to have_link "Followers", href: followers_user_path(user1)
       end
 
       it "正しいタイトルが表示される事" do
         expect(page).to have_title full_title("Following")
       end
 
-      it "ユーザー情報が表示されていること" do
+      it "ユーザー名、フォロー数、フォロワー数が表示される" do
         expect(page).to have_content user1.name
-        expect(page).to have_link "Profile", href: user_path(user1)
-        expect(page).to have_content "行き先 #{user1.destinations.count}"
-        expect(page).to have_link "#{user1.following.count} following", href: following_user_path(user1)
-        expect(page).to have_link "#{user1.followers.count} followers", href: followers_user_path(user1)
+        expect(page).to have_content "フォロー中 #{user1.following.count}人"
+        expect(page).to have_content "フォロワー #{user1.followers.count}人"
       end
 
-      it "フォロー中のユーザーが表示されていること" do
-        within('.users') do
-          expect(page).to have_css 'li', count: user1.following.count
-          user1.following.each do |u|
-            expect(page).to have_link u.name, href: user_path(u)
-          end
+      it "フォロー中のユーザの表示件数が正しく表示される" do
+        within('.user-follow-list-wrapper') do
+          expect(page).to have_css 'li.user-follow-list-user', count: user1.following.count
+        end
+      end
+
+      it "フォロー中のユーザ名、自己紹介が表示される" do
+        user1.following.each do |u|
+          expect(page).to have_link u.name, href: user_path(u)
+          expect(page).to have_content u.introduction
         end
       end
     end
   end
 
   describe "users#followers" do
-    before do
-      create(:relationship, follower_id: user2.id, followed_id: user1.id)
-      create(:relationship, follower_id: user3.id, followed_id: user1.id)
-      create(:relationship, follower_id: user4.id, followed_id: user1.id)
-      login_for_system(user1)
-      visit followers_user_path(user1)
-    end
+    context "ログインユーザがフォロー中一覧ページにアクセスした場合" do
+      let(:user1) { create(:user) }
+      let(:user2) { create(:user) }
 
-    context "ページレイアウト" do
-      it "'Followers'の文字列が存在すること" do
-        expect(page).to have_content "Followers"
+      before do
+        create(:relationship, follower_id: user2.id, followed_id: user1.id)
+        login_for_system(user1)
+        visit followers_user_path(user1)
       end
 
-      it "正しいタイトルが表示されること" do
+      it "'Following / Followers'が表示される" do
+        expect(page).to have_content "Following / Followers"
+      end
+
+      it "users#following へのリンクが表示される" do
+        expect(page).to have_link "Following", href: following_user_path(user1)
+      end
+
+      it "正しいタイトルが表示される事" do
         expect(page).to have_title full_title("Followers")
       end
 
-      it "ユーザー情報が表示されていること" do
+      it "ユーザー名、フォロー数、フォロワー数が表示される" do
         expect(page).to have_content user1.name
-        expect(page).to have_link "Profile", href: user_path(user1)
-        expect(page).to have_content "行き先 #{user1.destinations.count}"
-        expect(page).to have_link "#{user1.following.count} following", href: following_user_path(user1)
-        expect(page).to have_link "#{user1.followers.count} followers", href: followers_user_path(user1)
+        expect(page).to have_content "フォロー中 #{user1.following.count}人"
+        expect(page).to have_content "フォロワー #{user1.followers.count}人"
       end
 
-      it "フォロワーが表示されていること" do
-        within('.users') do
-          expect(page).to have_css 'li', count: user1.followers.count
-          user1.followers.each do |u|
-            expect(page).to have_link u.name, href: user_path(u)
-          end
+      it "フォロワーのユーザの表示件数が正しく表示される" do
+        within('.user-follow-list-wrapper') do
+          expect(page).to have_css 'li.user-follow-list-user', count: user1.followers.count
+        end
+      end
+
+      it "フォロー中のユーザ名、自己紹介が表示される" do
+        user1.followers.each do |u|
+          expect(page).to have_link u.name, href: user_path(u)
+          expect(page).to have_content u.introduction
         end
       end
     end
   end
 
   describe "フィード" do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
+    let(:destination1) { create(:destination, user: user1) }
+    let(:destination2) { create(:destination, user: user2) }
+    let(:destination3) { create(:destination, user: user3) }
+
     before do
       create(:relationship, follower_id: user1.id, followed_id: user2.id)
       login_for_system(user1)
