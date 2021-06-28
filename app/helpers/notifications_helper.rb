872b70788e707ff84af89c87bf516172cdb0ca53
karlley <karlley.jp@gmail.com>
2021-06-29 07:27:21 +0900

    Fix: notifications#index ユーザ/旅先取得 ヘルパー化
    view ファイルのユーザ/旅先取得 処理をヘルパー化

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,11 @@
 module NotificationsHelper
+  # 通知元のユーザを取得
+  def get_user_for_notification(notification)
+    User.find(notification.from_user_id)
+  end
+
+  # 通知元の旅先を取得
+  def get_destination_for_notification(notification)
+    Destination.find(notification.destination_id)
+  end
 end
