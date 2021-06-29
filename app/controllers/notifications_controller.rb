bfa14fe06ff634427bb54632a146275c508d72db
karlley <karlley.jp@gmail.com>
2021-06-30 05:39:07 +0900

    Fix: notifications#index 表示スタイル 修正
    通知タイプアイコン表示、ユーザアイコン表示、sp用スタイル追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    # current_user の通知を取得
+    # current_user の通知を全件取得
     @notifications = current_user.notifications
     # Notifications ページで通知確認後に通知フラグを削除
     current_user.update_attribute(:notification, false)
