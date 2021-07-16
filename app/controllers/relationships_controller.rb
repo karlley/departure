52000a0050b4614fa03da1ec4565bf2d2a20ac4d
karlley <karlley.jp@gmail.com>
2021-07-16 09:27:04 +0900

    Fix: users/show, show_follow フォロー機能 Ajax化
    relationships#create/#destroy, js.erb 共通化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,49 +2,51 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    # users/_list_follow から送信されたrelationship_user.id を:follow_idとしてフォローするユーザを取得
-    @follow_user = User.find(params[:followed_id])
-    current_user.follow(@follow_user)
-    # user/_list_follow から送信されたcurrent_page_user.id で表示するユーザを取得
-    @user = User.find(params[:current_page_user_id])
-    respond_to do |format|
-      # show/show-follow で分岐させる必要有り
-      format.html { redirect_to @follow_user }
-      format.js
+    if params[:current_page_user_id].blank?
+      # users/show
+      # :followed_id からフォローするユーザを取得
+      @follow_user = User.find(params[:followed_id])
+      current_user.follow(@follow_user)
+      respond_to do |format|
+        format.html { redirect_to @follow_user }
+        format.js
+      end
+    elsif
+      # users/show_follow
+      # :followed_id からフォローするユーザを取得
+      @follow_user = User.find(params[:followed_id])
+      current_user.follow(@follow_user)
+      # user/_list_follow から送信されたcurrent_page_user.id で表示するユーザを取得
+      @user = User.find(params[:current_page_user_id])
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
     end
   end
 
   def destroy
-    # users/_list_unfollow から送信されたrelationship_user.id を:follow_idとしてフォロー済みデータからアンフォローするユーザを取得
-    @unfollow_user = Relationship.find(params[:id]).followed
-    current_user.unfollow(@unfollow_user)
-    # user/_list_follow から送信されたcurrent_page_user.id で表示するユーザを取得
-    @user = User.find(params[:current_page_user_id])
-    respond_to do |format|
-      # show/show-follow で分岐させる必要有り
-      format.html { redirect_to @unfollow_user }
-      format.js
+    if params[:current_page_user_id].blank?
+      # users/show
+      # フォロー済みデータからアンフォローするユーザを取得
+      @unfollow_user = Relationship.find(params[:id]).followed
+      current_user.unfollow(@unfollow_user)
+      respond_to do |format|
+        format.html { redirect_to @unfollow_user }
+        format.js
+      end
+    elsif
+      # users/show_follow
+      # フォロー済みデータからアンフォローするユーザを取得
+      @unfollow_user = Relationship.find(params[:id]).followed
+      @unfollow_user = Relationship.find(params[:id]).followed
+      current_user.unfollow(@unfollow_user)
+      # user/_list_follow から送信されたcurrent_page_user.id で表示するユーザを取得
+      @user = User.find(params[:current_page_user_id])
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
     end
   end
-
-  # def create
-  #   @user = User.find(params[:followed_id])
-  #   current_user.follow(@user)
-  #   respond_to do |format|
-  #     # show/show-follow で分岐させる必要有り
-  #     format.html { redirect_to @user }
-  #     format.js
-  #   end
-  # end
-
-  # def destroy
-  #   @user = Relationship.find(params[:id]).followed
-  #   current_user.unfollow(@user)
-  #   respond_to do |format|
-  #     # show/show-follow で分岐させる必要有り
-  #     format.html { redirect_to @user }
-  #     format.js
-  #   end
-  # end
-
 end
