61f384efd6d785a30c80a87d751483a48f267f6e
karlley <karlley.jp@gmail.com>
2021-07-29 13:06:11 +0900

    Remove: Destination country カラム削除
    外部キー用にcountry_id カラムを追加したので不要

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_212335) do
+ActiveRecord::Schema.define(version: 2021_07_29_030908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_07_27_212335) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
-    t.string "country"
     t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
