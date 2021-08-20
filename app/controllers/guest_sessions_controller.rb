2dd423533ba25cf79e7df3cb3750d1597dadf98d
karlley <karlley.jp@gmail.com>
2021-08-20 10:32:30 +0900

    Improve: 簡単ログイン時の変数名を修正
    user > guest_user に修正

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -3,15 +3,15 @@ class GuestSessionsController < ApplicationController
   include SessionsHelper
 
   def create
-    user = User.find_or_create_by(email: "guest@example.com") do |u|
-      u.password = SecureRandom.urlsafe_base64
-      u.name = "ゲストユーザー"
-      u.introduction = "ゲストユーザーです！\n機能を試してみてください！"
-      u.nationality = "日本"
+    guest_user = User.find_or_create_by(email: "guest@example.com") do |g|
+      g.password = SecureRandom.urlsafe_base64
+      g.name = "ゲストユーザー"
+      g.introduction = "ゲストユーザーです！\n機能を試してみてください！"
+      g.nationality = "日本"
     end
-    log_in user
-    remember user
-    flash[:success] = "ゲストユーザーでログインしました"
+    log_in guest_user
+    remember guest_user
+    flash[:success] = "#{guest_user.name} でログインしました"
     redirect_to root_url
   end
 end
