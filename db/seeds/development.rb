933ebb6009191c337182ff1e7b3d957a000513e9
karlley <karlley.jp@gmail.com>
2021-08-23 02:25:19 +0900

    Fix: seed データ 日本語化、投稿画像 追加
    座標、住所、国名はとりあえずFaker でランダム

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,5 +1,10 @@
-# Country CSV import
+# Faker を日本語に設定
+Faker::Config.locale = :ja
+
+# CSV import
 # Destination のseed が作成できないので先にCountry, Airline を作成
+
+# Country
 require "csv"
 
 CSV.foreach("country.csv", headers: true) do |row|
@@ -9,7 +14,7 @@ CSV.foreach("country.csv", headers: true) do |row|
   )
 end
 
-# Airline CSV import
+# Airline
 CSV.foreach("airline.csv", headers: true) do |row|
   Airline.create!(
     airline_name: row["航空会社"],
@@ -19,48 +24,76 @@ CSV.foreach("airline.csv", headers: true) do |row|
   )
 end
 
-# Admin User
-User.create!(name: "Example User",
-             email: "sample@example.com",
+# User
+
+# Admin
+User.create!(name: "管理人",
+             email: "admin@example.com",
              password: "password",
              password_confirmation: "password",
-             introduction: "My name is Example User!",
-             nationality: "Japan",
+             introduction: "管理人です。",
+             nationality: "日本",
              admin: true)
 
-# User
-99.times do |n|
+# General User
+3.times do |n|
   name = Faker::Name.name
-  email = "sample-#{n + 1}@example.com"
+  email = "general-#{n + 1}@example.com"
   password = "password"
+  introduction = "#{n + 1}番目の一般ユーザーです！"
+  nationality = "日本"
   User.create!(name: name,
                email: email,
                password: password,
                password_confirmation: password,
-               introduction: "My name is Faker No.#{n + 1}!",
-               nationality: Faker::Address.country)
+               introduction: introduction,
+               nationality: nationality)
 end
 
 # Destination
-10.times do |n|
-  Destination.create!(name: Faker::Address.city,
-                      description: "This is Faker Destination No.#{n + 1}!",
-                      # seed で作成したCountry オブジェクトの中からランダムに国を選択
-                      country_id: Country.find(Faker::Number.between(from: 1, to: 249)).id,
-                      spot: Faker::Address.street_name,
-                      latitude: Faker::Address.latitude,
-                      longitude: Faker::Address.longitude,
-                      # address: Geocoder.search([:latitude, :longitude]).first.address,
-                      address: Faker::Address.full_address,
-                      # :expense のenum 表示に合わせて1-8 の数字を生成,
-                      expense: Faker::Number.between(from: 1, to: 8),
-                      season: Faker::Number.between(from: 1, to: 12),
-                      experience: "Your Experience!",
-                      # seed で作成したAirline オブジェクトの中からランダムに国を選択
-                      airline_id: Airline.find(Faker::Number.between(from: 1, to: 90)).id,
-                      food: Faker::Food.dish,
-                      user_id: 1)
-end
+
+  10.times do |n|
+
+    picture = File.open("#{Rails.root}/app/assets/images/sample/sample_#{Faker::Number.between(from: 1, to: 10)}.jpg")
+    # 旅先名のリスト
+    name = "サンプル #{n + 1}の旅先"
+    description = "これは#{n + 1}番目の旅先です！"
+    # :expense のenum 表示に合わせて1-8 の数字を生成,
+    expense = Faker::Number.between(from: 1, to: 8)
+    season = Faker::Number.between(from: 1, to: 12)
+    # seed で作成したCountry オブジェクトの中からランダムに国を選択
+    country_id =  Country.find(Faker::Number.between(from: 1, to: 249)).id
+    spot = Faker::Address.street_name
+    latitude = Faker::Address.latitude
+    longitude = Faker::Address.longitude
+    address = Faker::Address.full_address
+    # :expense のenum 表示に合わせて1-8 の数字を生成,
+    expense = Faker::Number.between(from: 1, to: 8)
+    season = Faker::Number.between(from: 1, to: 12)
+    # 体験の中からランダムに選択
+    experience_list = ["アクティビティ", "歴史", "一人旅"]
+    experience = experience_list[(Faker::Number.between(from: 0, to: 2))]
+    # seed で作成したAirline オブジェクトの中からランダムに航空会社を選択
+    airline_id = Airline.find(Faker::Number.between(from: 1, to: 90)).id
+    food = Faker::Food.dish
+    # 管理人は含まない
+    user_id = Faker::Number.between(from: 2, to: 4)
+
+    Destination.create!(picture: picture,
+                        name: name,
+                        description: description,
+                        country_id: country_id,
+                        spot: spot,
+                        latitude: latitude,
+                        longitude: longitude,
+                        address: address,
+                        expense: expense,
+                        season: season,
+                        experience: experience,
+                        airline_id: airline_id,
+                        food: food,
+                        user_id: user_id)
+  end
 
 # Relationship
 users = User.all
