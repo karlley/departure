f135028f4f91f7913ff958c7e4f7dd40afebdc11
karlley <karlley.jp@gmail.com>
2021-08-27 05:02:42 +0900

    Add: AWS S3 に画像を保存する設定を追加
    プリコンパイル設定、CarrierWave 設定、AWS 環境変数設定を追加

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,8 +4,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  # production 環境ではクラウドストレージに画像を保存する
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
