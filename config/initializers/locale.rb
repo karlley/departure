710ce91f38a514fdbacddcc3f5f650e9f2c340a1
karlley <karlley.jp@gmail.com>
2021-09-18 05:01:47 +0900

    Fix: enum 日本語化 設定追加、locale ファイル修正
    ロケールファイルの読込設定追加、enum 翻訳情報追加

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,8 @@
-Rails.application.config.i18n.default_locale = :ja
+# locals ファイル読込ディレクトリ
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+# デフォルト言語
+I18n.default_locale = :ja
+
+# アプリケーションで有効化する言語指定
+I18n.available_locales = :ja
